13b042bdeade84c78dc82958da7506ae842238fd
Vladimir Kuznetsov <asfvova6@gmail.com>
2025-07-11 21:26:49 +0300

    2 задачи

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length>maxlength){
+    return str.substring(0,maxlength - 1) +'…';
+  }
+  return str;
 }
+console.log (truncate("какой прекрасный день сейчас", 20));
+console.log (truncate("какой день"));
+console.log (truncate("какой прекрасный день", 10));
