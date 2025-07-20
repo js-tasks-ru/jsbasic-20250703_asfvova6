a7fe188b4df1bf30f48de71582250066f00949bb
Vladimir Kuznetsov <asfvova6@gmail.com>
2025-07-20 15:29:08 +0300

    3 задание

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,7 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+ return arr.filter(function (item){
+  return item >= a && item <= b ;
+})
 }
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr , 1 , 9)
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,24 @@
 function showSalary(users, age) {
-  // ваш код...
+  let SalariName = users.filter (users =>users.age <= age);
+  let names = SalariName.map(users =>`${users.name}, ${users.balance}`).join('\n');
+  return names;
 }
+  let users1 = {
+  balance: "$1,825.00",
+  age: 30,
+  name: "Not Valera",
+  gender: "male",
+  greeting: "Hello, Golden Branch! You have 7 unread messages.",
+  favouriteFruit: "banana"
+};
+let users2 = {
+  balance: "$1,825.65",
+  age: 40,
+  name: "Valera",
+  gender: "male",
+  greeting: "Hello, Golden Branch! You have 7 unread messages.",
+  favouriteFruit: "banana"
+};
+const age = 40;
+let users =[users1 , users2];
+console.log (showSalary(users , age));
